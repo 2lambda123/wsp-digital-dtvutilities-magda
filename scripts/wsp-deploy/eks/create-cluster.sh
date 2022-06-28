f97ab4b9248aa6de45605aa648db05a94dc3d9aa
Paul Borg <paul.borg@wsp.com>
2022-06-28 10:51:30 +1000

    DevOps - Add deployment for the Kubernetes Dashboard to scripts

--- scripts/wsp-deploy/eks/create-cluster.sh
+++ scripts/wsp-deploy/eks/create-cluster.sh
@@ -9,3 +9,6 @@ kubectl apply -f https://s3.us-west-2.amazonaws.com/amazon-eks/docs/eks-console-
 
 eksctl create iamidentitymapping --cluster $CLUSTER_NAME --arn arn:aws:iam::940728446396:role/wspdigital-administrator --group eks-console-dashboard-full-access-group --no-duplicate-arns
 eksctl create iamidentitymapping --cluster $CLUSTER_NAME --arn arn:aws:iam::940728446396:role/wspdigital-poweruser --group eks-console-dashboard-full-access-group --no-duplicate-arns
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.5/aio/deploy/recommended.yaml
+kubectl apply -f eks-admin-service-account.yaml
