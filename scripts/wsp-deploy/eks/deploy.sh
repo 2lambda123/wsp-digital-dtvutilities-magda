d6606637829839d4f5db5ff22a4638393dfae72e
Paul Borg <paul.borg@wsp.com>
2022-06-28 10:50:52 +1000

    DevOps - Deploy the Magda Web Server from ECR

--- scripts/wsp-deploy/eks/deploy.sh
+++ scripts/wsp-deploy/eks/deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+IMAGE_TAG=${1:latest}
+
 cd ../../..
 
-helm upgrade --namespace magda --install --timeout 9999s magda deploy/helm/wsp-magda -f deploy/helm/wsp-deploy-test.yml
+helm upgrade --namespace magda --install --timeout 9999s magda deploy/helm/wsp-magda -f deploy/helm/wsp-deploy-test.yml --set "magda.magda-core.web-server.image.tag=${IMAGE_TAG}"
 
 cd -
