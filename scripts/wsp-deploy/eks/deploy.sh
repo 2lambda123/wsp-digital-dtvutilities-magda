e0f94c63e9274d99fd8640b91de949722b413450
Paul Borg <paul.borg@wsp.com>
2022-06-29 17:18:13 +1000

    DevOps - Add K8s GitHub Action Role, Rolebinding and Docker deploy

--- scripts/wsp-deploy/eks/deploy.sh
+++ scripts/wsp-deploy/eks/deploy.sh
@@ -2,8 +2,13 @@
 
 IMAGE_TAG=${1:latest}
 
-cd ../../..
+TMPFILE=$(mktemp)
 
+echo ${KUBECONFIG} | base64 -d > ${TMPFILE}
+export KUBECONFIG="${TMPFILE}"
+chmod 600 ${TMPFILE}
+
+helm dependency update deploy/helm/wsp-magda
 helm upgrade --namespace magda --install --timeout 9999s magda deploy/helm/wsp-magda -f deploy/helm/wsp-deploy-test.yml --set "magda.magda-core.web-server.image.tag=${IMAGE_TAG}"
 
-cd -
+rm -f "${TMPFILE}"
