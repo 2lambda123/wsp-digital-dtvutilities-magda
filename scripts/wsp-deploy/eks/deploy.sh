7b443bb181cf2ae1630586c44429c459c62e9d3e
Paul Borg <paul.borg@wsp.com>
2022-06-29 18:25:50 +1000

    DevOps - add back base64 decode

--- scripts/wsp-deploy/eks/deploy.sh
+++ scripts/wsp-deploy/eks/deploy.sh
@@ -4,13 +4,9 @@ IMAGE_TAG=${1:-$(git rev-parse --short HEAD)}
 
 TMPFILE=$(mktemp)
 
-echo ${KUBECONFIG} > ${TMPFILE}
+echo ${KUBECONFIG} | base64 -d > ${TMPFILE}
 export KUBECONFIG="${TMPFILE}"
 chmod 600 ${TMPFILE}
-
-echo "------------------------------------------KUBECONFIG------------------------------------------"
-cat ${TMPFILE}
-echo "------------------------------------------KUBECONFIG------------------------------------------"
 
 helm dependency update deploy/helm/wsp-magda
 helm upgrade --namespace magda --install --timeout 9999s magda deploy/helm/wsp-magda -f deploy/helm/wsp-deploy-test.yml --set "magda.magda-core.web-server.image.tag=${IMAGE_TAG}"
