c7da2ee87a6f26fe3aee46977d390813534a9fc1
Paul Borg <paul.borg@wsp.com>
2022-06-29 18:05:53 +1000

    DevOps - wait ... cat the KUBECONFIG file

--- scripts/wsp-deploy/eks/deploy.sh
+++ scripts/wsp-deploy/eks/deploy.sh
@@ -8,6 +8,8 @@ echo ${KUBECONFIG} > ${TMPFILE}
 export KUBECONFIG="${TMPFILE}"
 chmod 600 ${TMPFILE}
 
+cat ${TMPFILE}
+
 helm dependency update deploy/helm/wsp-magda
 helm upgrade --namespace magda --install --timeout 9999s magda deploy/helm/wsp-magda -f deploy/helm/wsp-deploy-test.yml --set "magda.magda-core.web-server.image.tag=${IMAGE_TAG}"
 
