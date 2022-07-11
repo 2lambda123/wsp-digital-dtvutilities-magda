d87aacc0329f9311f1409b94771439773ef7403a
peter_high <peter.high@wspdigital.com>
2022-07-12 09:56:33 +1000

    DTV - test - phigh - Added install scrip to install runner controller and runners

--- scripts/wsp-deploy/eks/install-github-acr.sh
+++ scripts/wsp-deploy/eks/install-github-acr.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 
+# create namespace
+kubectl create namespace actions-runner-system
+
 # creates secret which will apply the app and installation id to a kube secret
 kubectl create secret generic controller-manager \
 -n actions-runner-system \
 --from-literal=github_app_id=218756 \
 --from-literal=github_app_installation_id=27277385 \
---from-literal=github_token=AI3YOIZFPRV64REQ535RSBLCZOWH2 \
 --from-file=github_app_private_key=wsp-dtv-magda-test.2022-07-10.private-key.pem
 
-helm upgrade --install --namespace actions-runner-system --create-namespace \
+# Add actions-runner-controller helm chart
+helm repo add actions-runner-controller https://actions-runner-controller.github.io/actions-runner-controller
+helm repo update
+
+
+# Install the actions-runner-controller
+helm upgrade --install --namespace actions-runner-system \
              --wait actions-runner-controller actions-runner-controller/actions-runner-controller \
              --version 0.20.1 \
              --set syncPeriod=3m
 
-
-helm install runner \
-actions-runner-controller/actions-runner-controller \
---namespace actions-runner-system \
---version 0.20.1 \
---set syncPeriod=3m
+# Install the runners themselves
+kubectl apply -f gh-runner.yml
